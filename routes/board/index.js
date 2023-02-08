85e415bcfe305162682306e4ae8bc95c1f254811
RyanOliverV <ryanoliverv@gmail.com>
2023-02-08 21:11:34 +0000

    m

---
+++ routes/board/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('board');
+})
+
+router.get('/:id', (req, res) => {
+    res.render('board');
+})
+
+module.exports = router;
